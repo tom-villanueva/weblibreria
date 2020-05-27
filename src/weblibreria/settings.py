e3ed7414d993ce3c32236dcb1f694ffcc1903801
Lautaro Barba <lau_mb94@hotmail.com>
2020-05-27 19:19:25 -0300

    Agregado enlaces para usuarios en template/base

--- src/weblibreria/settings.py
+++ src/weblibreria/settings.py
@@ -123,10 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIR = (
-    os.path.join(BASE_DIR, 'static')
-)
+#STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
