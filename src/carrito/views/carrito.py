d40b1f1a13e49574416b52017eb4a42fb4ba45ab
tom-villanueva <54720888+tom-villanueva@users.noreply.github.com>
2020-05-30 05:01:21 -0300

    carrito sin pk en la barra

--- src/carrito/views/carrito.py
+++ src/carrito/views/carrito.py
@@ -1,6 +1,17 @@
 from django.views.generic import DetailView
 from carrito.models import Carrito
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class CarritoDetailView(DetailView):
+class CarritoDetailView(LoginRequiredMixin, DetailView):
     template_name = 'carrito/carrito_detail.html' 
     model = Carrito
+
+    login_url = '/users/login/'
+
+    def get(self, request, *args, **kwargs):
+        self.carrito_id = request.user.carrito.id
+        print(self.carrito_id)
+        return super().get(request, *args, **kwargs)
+
+    def get_object(self):
+        return Carrito.objects.get(id=self.carrito_id)
--- src/weblibreria/urls.py
+++ src/weblibreria/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('libreria.urls')),
     path('users/', include('users.urls')),
-    path('carrito/<int:pk>', include('carrito.urls')),
+    path('carrito/', include('carrito.urls')),
 ]
