894ce10731ffc8732c2a5c1021109cb34acc2937
tom-villanueva <54720888+tom-villanueva@users.noreply.github.com>
2020-05-30 01:40:36 -0300

    generos libros

--- src/libreria/urls.py
+++ src/libreria/urls.py
@@ -4,7 +4,7 @@ from libreria.views import BookDetailView, GenreListView, BookListView
 
 urlpatterns = [
     path('', VistaHome.as_view(), name='home'),
-    path('Libro/<int:pk>/', BookDetailView.as_view(), name='book-view'),
-    path('Generos/', GenreListView.as_view(), name='genre-list-view'),
-    path('Generos/<slug:slug>/', BookListView.as_view(), name='book-list-view'),  
+    path('libro/<int:pk>/', BookDetailView.as_view(), name='book-view'),
+    path('generos/', GenreListView.as_view(), name='genre-list-view'),
+    path('generos/<slug:slug>/', BookListView.as_view(), name='book-list-view'),  
 ]
--- src/libreria/views/booklistview.py
+++ src/libreria/views/booklistview.py
@@ -4,4 +4,14 @@ from libreria.models.book import Book
 class BookListView(ListView):
     model = Book 
 
-    
+    def get(self, request, *args, **kwargs):
+        self.genero = self.kwargs['slug']
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['genero'] = self.genero
+        return context
+
+    def get_queryset(self):
+        return Book.objects.filter(genre = self.genero)[:5]
